48aec3ae18e77e703097227b38883ffc54349286
liangmayong <ibeam@qq.com>
2016-09-18 16:55:09 +0800

    upload

---
+++ appbox/src/main/java/com/liangmayong/appbox/core/AppboxCore.java
@@ -0,0 +1,7 @@
+package com.liangmayong.appbox.core;
+
+/**
+ * Created by LiangMaYong on 2016/9/18.
+ */
+public class AppboxCore {
+}
---
+++ appbox/src/main/java/com/liangmayong/appbox/support/AppboxSupport.java
@@ -0,0 +1,7 @@
+package com.liangmayong.appbox.support;
+
+/**
+ * Created by LiangMaYong on 2016/9/18.
+ */
+public class AppboxSupport {
+}
